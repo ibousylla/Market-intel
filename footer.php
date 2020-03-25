314a00f2a7cf238191b6bb4ad8b0affc13a5f913
ibousylla <ibousylla1206@gmail.com>
2020-03-25 11:14:59 +0000

    Market-intel

---
+++ footer.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Identification</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="/your-path-to-fontawesome/css/all.css" rel="stylesheet">
+  <link rel="stylesheet" href="login.css"/>  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+ 
+  
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  
+</head>
+<body>
+
+<footer class="page-footer font-small blue " >
+
+<div class="container-fluid">
+    <div class="row">
+
+    <div class="col-md-4">
+        <div>
+            <h3>MARKET-INTEL</h3>
+        </div>
+        <div>
+            <h3>Rester connecté</h3>
+        </div>
+        <div>
+            <h3>Market-Intel</h3>
+        </div>
+        <div>
+            <h3>Abonnez-vous au Market-Intel News</h3>
+        </div>
+    </div>
+
+    <div class="col-md-4">
+        <div> 
+            <h3> Qui nous sommes </h3>
+        </div>
+        <div> 
+            <h4> Market-Intel est une application web qui facilite la prise de décision, à travers une visualisation d'ensemble sur le marché sénégalais, en premier lieu et en Afrique en général </h4>
+        </div>
+    </div>
+
+    <div class="col-md-4">
+        <div> 
+            <h3> Ce que nous faisons </h3>
+        </div>
+        <div> 
+            <h4> C'est un outil destiné aux jeunes entrepreuneurs qui veuleunt avoir une idée sur les données concernant un secteur d'activité, sur les problèmatiques sur la place d'un marcé dans un pays  </h4>
+        </div>
+    </div>
+
+
+    </div>
+
+</div>
+
+</footer>
+    
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Identification</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="/your-path-to-fontawesome/css/all.css" rel="stylesheet">
+  <link rel="stylesheet" href="login.css"/>  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+ 
+  
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  
+</head>
+<body>
+
+    
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+       
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <a class="navbar-brand" href="#">MARKET-INTEL</a>
+          <div class="form-group has-search">
+            <span class="fa fa-search form-control-feedback"></span>
+            <input type="text" class="form-control" placeholder="Search">
+          </div>
+          
+          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">MENU</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">RESSOURCES</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">S'IDENTIFIER</a>
+            </li>
+          </ul>
+         
+        </div>
+      </nav>
+
+</body>
+</html>
